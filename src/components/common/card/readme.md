09595070358ccc2989109b8b6286037caa4b3747
Amybiubiu <1246457100@qq.com>
2019-10-11 10:29:19 +0800

    revamp

---
+++ src/components/common/card/readme.md
@@ -0,0 +1,9 @@
+#### 组件名：card
+
+#### 使用示例代码
+
+```js
+// pages/index/index.jsx
+import { MxCard } from "../../components/card";
+<MxCard>这里装的是卡片内容</MxCard>；
+```
--- src/components/common/picker/index.jsx
+++ src/components/common/picker/index.jsx
@@ -2,7 +2,7 @@ import Taro from '@tarojs/taro'
 import PropTypes from 'prop-types'
 // import classNames from 'classnames'
 import {  Picker, View } from '@tarojs/components'
-import { MxIcon } from './icon'
+import { MxIcon } from '../icon'
 import MxComponent from '../../common/component'
 import './index.scss'
 
@@ -18,7 +18,12 @@ render () {
     const {
       selectorChecked,
       selector,
+      width,
+      customStyle,
     } = this.props
+    const rootStyle = {
+      width: width,
+    }
 
     return (
       <View className='container'>
@@ -26,7 +31,7 @@ render () {
           <View className='page-section'>
             <View>
               <Picker mode='selector' range={selector} onChange={this.handleChange.bind(this)}>
-                <View className='picker'>
+                <View className='picker' style={this.mergeStyle(rootStyle, customStyle)}>
                   { selectorChecked }
                 </View>
                 <MxIcon value='arrow-up' className='icon'></MxIcon>
@@ -41,11 +46,15 @@ render () {
 MxPicker.defaultProps = {
   selector: [],
   selectorChecked: '',
+  width: '100px',
+  customStyle: '',
   onChange () {},
 }
 
 MxPicker.propTypes = {
   selector: PropTypes.array,
   selectorChecked: PropTypes.string,
+  width: PropTypes.string,
+  customStyle: PropTypes.string,
   onChange: PropTypes.func,
 }
