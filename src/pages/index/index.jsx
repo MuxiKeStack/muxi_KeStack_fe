7541a84ecf3afea396a3983afc702e564b66c58d
xinhui.zxh <xinhui.zxh@antfin.com>
2019-10-16 14:35:46 +0800

    feat: update MxCard

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -6,11 +6,12 @@ import MxRate from '../../components/common/MxRate/MxRate'
 import Ring from '../../components/page/Ring/Ring'
 import MuxibuttonN from '../../components/common/nomalButton'
 import MuxibuttonI from '../../components/common/imgButton'
+import { MxCard } from "../../components/common/card";
 import pic from '../../assets/png/good.png'
 
 export default class Index extends Component {
 
-  constructor () {
+  constructor() {
     super(...arguments)
     this.state = {
       value: 2.5,
@@ -26,7 +27,7 @@ export default class Index extends Component {
 
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
   config = {
     navigationBarTitleText: '首页'
   }
@@ -35,15 +36,15 @@ export default class Index extends Component {
 
   componentWillUnmount() { }
 
-  handleChange (value) {
+  handleChange(value) {
     this.setState({
       value,
     })
   }
 
-  componentDidHide () { }
+  componentDidHide() { }
   //a
-  render () {
+  render() {
     return (
       <View className='index'>
         <Text>{this.state.name}</Text>
@@ -61,10 +62,20 @@ export default class Index extends Component {
           show={true}
           value={this.state.value2}
         /> */}
+
+        <MxCard>这里装的是卡片内容</MxCard>；
         <MxRate
           value={this.state.value}
         />
-        <Ring />
+        <Ring
+          type='成绩'
+          percent1='13'
+          percent2='76'
+          percent3='11'
+          name1='经常点名'
+          name2='偶尔点名'
+          name3='从不点名'
+        />
         <MuxibuttonN />
         <MuxibuttonI src={pic}></MuxibuttonI>
       </View>
