188c53bad4a61166785c5fd24e623afb4a50f749
Moomyd <1345449751@qq.com>
2019-12-15 17:14:34 +0800

    fix login tag fab search courseAssitant

--- src/pages/courseAssistant/index.jsx
+++ src/pages/courseAssistant/index.jsx
@@ -1,22 +1,20 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Text, Textarea } from '@tarojs/components'
-import './index.scss'
-import MxCard from '../../components/common/MxCard'
-import MxRate from '../../components/common/MxRate/MxRate'
-import MxIcon from '../../components/common/MxIcon'
-import octodex from '../../assets/png/octodex.jpg'
-import MxInput from '../../components/common/MxInput/MxInput'
-import {MxFab} from '../../components/common/MxFab/index'
-export default class Index extends Component {
+import Taro, { Component } from '@tarojs/taro';
+import { View, Text } from '@tarojs/components';
+import './index.scss';
+import MxInput from '../../components/common/MxInput/MxInput';
+import MxFab from '../../components/common/MxFab';
 
+export default class Index extends Component {
+  // eslint-disable-next-line react/sort-comp
   config = {
-    navigationBarTitleText: '选课助手',
-  }
+    navigationBarTitleText: '选课助手'
+  };
 
   constructor() {
     super(...arguments);
     this.state = {
-      value:5,
+      // eslint-disable-next-line react/no-unused-state
+      value: 5,
       datas: [
         {
           text: '马克思主义基本原理',
@@ -24,7 +22,7 @@ export default class Index extends Component {
           num: '2019347817462',
           people: '23',
           time: '周二7~8；周四5~6',
-          address: '7203(东区)',
+          address: '7203(东区)'
         },
         {
           text: '线性代数B',
@@ -32,7 +30,7 @@ export default class Index extends Component {
           num: '2019347817462',
           people: '23',
           time: '周二7~8；周四5~6',
-          address: '7203(东区)',
+          address: '7203(东区)'
         },
         {
           text: '线性代数B',
@@ -40,92 +38,91 @@ export default class Index extends Component {
           num: '2019347817462',
           people: '23',
           time: '周二7~8；周四5~6',
-          address: '7203(东区)',
-        },
-       
+          address: '7203(东区)'
+        }
       ]
     };
   }
 
-  handleChange (value) {
+  handleChange(value) {
     this.setState({
+      // eslint-disable-next-line react/no-unused-state
       value
-    })
+    });
   }
 
   ChangeTodetails() {
     Taro.navigateTo({
-        url: '/pages/courseDetails/courseDetails'
+      url: '/pages/courseDetails/courseDetails'
     });
   }
 
-
   ChangeTofree() {
     Taro.navigateTo({
-        url: '/pages/freeCourse/index'
+      url: '/pages/freeCourse/index'
     });
-}
-  componentWillMount() { }
+  }
 
-  componentDidMount() { }
+  componentWillMount() {}
 
-  componentWillUnmount() { }
+  componentDidMount() {}
 
-  componentDidShow() { }
+  componentWillUnmount() {}
 
-  componentDidHide() { }
+  componentDidShow() {}
+
+  componentDidHide() {}
 
   render() {
     const content = (
-      <View className='detailsBoxes'>
-      <View
-        className='muxi-fab'
-        onClick={this.ChangeTofree.bind(this)}
-      >
-        排课
+      <View className="detailsBoxes">
+        <MxFab back>置顶</MxFab>
+        {this.state.datas.map(data => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <View className="detailsBox">
+              <View
+                className="mx-card"
+                onClick={this.ChangeTodetails.bind(this)}
+              >
+                <View className="info">
+                  <View className="user-info">
+                    <View className="class">{data.text}</View>
+                    <View className="teacher">{data.teacher}</View>
+                    <View className="num">{data.num}</View>
+                  </View>
+                  <View className="ta">
+                    <View className="time">
+                      <Text>{data.time}</Text>
+                    </View>
+                    <View className="address">
+                      <Text>{data.address}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          );
+        })}
       </View>
-        {
-          this.state.datas.map((data) => {
-            return (
-              <View className='detailsBox'>
-                <View className='mx-card' onClick={this.ChangeTodetails.bind(this)}>
-  
-        <View className='info'>
-        <View className='user-info'>
-            <View className='class'>{data.text}</View>
-            <View className='teacher'>{data.teacher}</View>
-            <View className='num'>{data.num}</View>
-        </View>
-        <View className='ta'> 
-          <View className='time'>
-              <Text>{data.time}</Text>
-          </View>
-          <View className='address'>
-              <Text>{data.address}</Text>
-          </View>
-        </View>
-        </View>
-        </View>
-      </View>
-            )
-          })
-        }
-      </View>
-    )
+    );
 
     return (
-      <View style='display: block'>
-        <View className='chooseBox'>
-          <View className='search'>
-          <MxInput
-          leftSrc='../../../assets/svg/searchicon.svg'leftSize='20px' width='670rpx' height='72rpx' background='rgba(241,240,245,1)'
-          radius='36rpx'
-          > 
-          </MxInput>
+      <View style="display: block">
+        <View className="chooseBox">
+          <View className="search">
+            <MxInput
+              leftSrc="../../../assets/svg/searchicon.svg"
+              leftSize="20px"
+              width="670rpx"
+              height="72rpx"
+              background="rgba(241,240,245,1)"
+              radius="36rpx"
+            ></MxInput>
           </View>
         </View>
         {content}
       </View>
-    )
+    );
   }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,13 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Text, Image } from '@tarojs/components';
+import { View, Text, Image, Button } from '@tarojs/components';
 import './index.scss';
 import MxInput from '../../components/common/MxInput/MxInput';
 import MxButton from '../../components/common/MxButton/index';
 import image from '../../assets/png/navigation.png';
 import Fetch from '../../service/fetch';
-import MxTag from '../../components/common/MxTag';
 
 export default class Index extends Component {
+  // eslint-disable-next-line react/sort-comp
   config = {
     navigationBarTitleText: '木犀课栈'
   };
@@ -16,44 +16,47 @@ export default class Index extends Component {
     this.state = {
       userid: '',
       password: '',
+      // eslint-disable-next-line react/no-unused-state
       value: 2.5,
-      username:"",
-      avatar:"",
+      // eslint-disable-next-line react/no-unused-state
+      username: '',
+      // eslint-disable-next-line react/no-unused-state
+      avatar: '',
       mask_name: 'unmask',
-      mask_bg: 'mask_bg_show',
-    }
+      mask_bg: 'mask_bg_show'
+    };
   }
 
-  handleSave(){
+  handleSave() {
     this.setState({
       mask_name: 'mask',
-      mask_bg: 'mask_bg_none',
-    })
+      mask_bg: 'mask_bg_none'
+    });
   }
 
-  handleMask(){
+  handleMask() {
     Taro.getSetting({
-      success:(res)=>{
-        if(res.authSetting['scope.userInfo']){
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
           this.setState({
             mask_name: 'mask',
-            mask_bg: 'mask_bg_none',
-          })
+            mask_bg: 'mask_bg_none'
+          });
         }
       },
-      fail:(res)=>{
+      fail: () => {
         this.setState({
           mask_name: 'unmask',
-          mask_bg: 'mask_bg_show',
-        })
+          mask_bg: 'mask_bg_show'
+        });
       }
-    })
+    });
   }
 
-  ChangeTo(){
+  ChangeTo() {
     Taro.switchTab({
       url: '/pages/commentSquare/index'
-    })
+    });
     // success=()=>{
     //   var page = Taro.getCurrentPages().pop();
     //   if (page == undefined || page == null) return;
@@ -62,7 +65,7 @@ export default class Index extends Component {
   }
   componentWillMount() {}
 
-  componentDidMount() { }
+  componentDidMount() {}
 
   componentWillUnmount() {}
 
@@ -84,10 +87,7 @@ export default class Index extends Component {
     });
   }
 
-
-  getUserInfo() {
-    var that = this;
-  }
+  getUserInfo() {}
   //20101密码错误
   //0 成功
   //500服务器错误
@@ -105,65 +105,66 @@ export default class Index extends Component {
       ).then(res => {
         switch (res.code) {
           case 0:
-              Taro.setStorage({
-                key:'token',
-                data:res.data.token,
-                success:function(res){
-                  Taro.getSetting({
-                    success(res){
-                      if(res.authSetting['scope.userInfo']){
-                        Taro.getUserInfo({
-                          success:function(res){
-                            Fetch(
-                              'api/v1/user/info',
-                              {
-                                avatar:res.userInfo.avatarUrl,
-                                username:res.userInfo.nickName
-                              },
-                              'POST'
-                            )
-                            Taro.switchTab({
-                              url:"/pages/commentSquare/index"
-                            });
-                          },
-                          fail:function(res){
-                            Taro.showToast({
-                              icon: 'none',
-                              title:'获取用户信息失败'
-                            })
-                          }
-                        })
-                      }else{
-                        Taro.showToast({
-                          icon: 'none',
-                          title:'请授权'
-                        })
-                      }
-                      
-                    },
-                    fail:function(res){
+            Taro.setStorage({
+              key: 'token',
+              data: res.data.token,
+              success: function() {
+                Taro.getSetting({
+                  // eslint-disable-next-line no-shadow
+                  success(res) {
+                    if (res.authSetting['scope.userInfo']) {
+                      Taro.getUserInfo({
+                        // eslint-disable-next-line no-shadow
+                        success: function(res) {
+                          Fetch(
+                            'api/v1/user/info',
+                            {
+                              avatar: res.userInfo.avatarUrl,
+                              username: res.userInfo.nickName
+                            },
+                            'POST'
+                          );
+                          Taro.switchTab({
+                            url: '/pages/commentSquare/index'
+                          });
+                        },
+                        fail: function() {
+                          Taro.showToast({
+                            icon: 'none',
+                            title: '获取用户信息失败'
+                          });
+                        }
+                      });
+                    } else {
                       Taro.showToast({
                         icon: 'none',
                         title: '请授权'
                       });
                     }
-                  })
-                }
-            })
+                  },
+                  fail: function() {
+                    Taro.showToast({
+                      icon: 'none',
+                      title: '请授权'
+                    });
+                  }
+                });
+              }
+            });
             break;
           case 20101:
-              Taro.showToast({
-                icon: 'none',
-                title:'账号或者密码错误'
-              })
+            Taro.showToast({
+              icon: 'none',
+              title: '账号或者密码错误'
+            });
             break;
         }
       });
     } else {
       Taro.showToast({
         icon: 'none',
-        title:'账号或密码不能为空'
-      })
+        title: '账号或密码不能为空'
+      });
     }
   }
 
@@ -191,26 +192,39 @@ export default class Index extends Component {
             onInput={this.changePassword.bind(this)}
           ></MxInput>
         </View>
-      <View className='login'>
-        <MxButton
-          buttomWidth='513rpx'
-          buttomHeight='92rpx'
-          buttonBackground='#6868F8'
-          border-radius='46rpx'
-          onClick={this.login.bind(this)}
-        >学号登录</MxButton>
-      </View>
-      
-      <View
-         className='visit'
-      ><Text className='visitor' onClick={this.ChangeTo.bind(this)}>游客登录</Text></View>
-    
-        <View className='privacy'><Text className='secret'>隐私条例</Text></View>
+        <View className="login">
+          <MxButton
+            buttomWidth="513rpx"
+            buttomHeight="92rpx"
+            buttonBackground="#6868F8"
+            border-radius="46rpx"
+            onClick={this.login.bind(this)}
+          >
+            学号登录
+          </MxButton>
+        </View>
+
+        <View className="visit">
+          <Text className="visitor" onClick={this.ChangeTo.bind(this)}>
+            游客登录
+          </Text>
+        </View>
+
+        <View className="privacy">
+          <Text className="secret">隐私条例</Text>
+        </View>
         <View className={this.state.mask_bg}></View>
         <View className={this.state.mask_name}>
-        <Button class='bottom' open-type="getUserInfo" onGetUserInfo={this.getUserInfo.bind(this)} onClick={this.handleSave.bind(this)}>
-         授权登陆
-        </Button>
+          <View>
+            <Button
+              class="bottom"
+              open-type="getUserInfo"
+              onGetUserInfo={this.getUserInfo.bind(this)}
+              onClick={this.handleSave.bind(this)}
+            >
+              授权登陆
+            </Button>
+          </View>
         </View>
       </View>
     );
